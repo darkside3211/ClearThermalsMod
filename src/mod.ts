4a7046e0d3c906a3a95851f5a5ae73e07e619de7
WebWarlock <gerarddominic.baldo@gmail.com>
2025-10-06 12:49:26 +0800

    2nd commit for debug

--- src/mod.ts
+++ src/mod.ts
@@ -1,18 +1,22 @@
 import { DependencyContainer } from "tsyringe";
+
+// SPT types
 import type { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
 import type { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import type { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import type { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 
-class ThermalMod implements IPreAkiLoadMod, IPostDBLoadMod {
-    private modName = "ClearThermalsMod";
-    private logger: ILogger | null = null;
+class ClearThermals implements IPreAkiLoadMod, IPostDBLoadMod
+{
+    private modName = "ClearThermals";
+    private logger: ILogger;
 
     /**
      * This method is called before SPT-AKI loads its data.
      * We use it to get a reference to the logger.
      */
-    public preAkiLoad(container: DependencyContainer): void {
+    public preAkiLoad(container: DependencyContainer): void 
+    {
         this.logger = container.resolve<ILogger>("WinstonLogger");
         this.logger.info(`[${this.modName}] Loading...`);
     }
@@ -21,45 +25,44 @@ class ThermalMod implements IPreAkiLoadMod, IPostDBLoadMod {
      * This method is called after SPT-AKI has loaded the database.
      * This is where we will make our changes to the thermal goggles.
      */
-    public postDBLoad(container: DependencyContainer): void {
-        if (!this.logger) {
+    public postDBLoad(container: DependencyContainer): void 
+    {
+        if (!this.logger) 
+        {
             this.logger = container.resolve<ILogger>("WinstonLogger");
         }
-        
+
         this.logger.info(`[${this.modName}] Database loaded. Applying thermal changes...`);
 
-        // Get the database server
+        // Get the DatabaseServer service
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
         
         // Get all the in-game items from the database
         const items = databaseServer.getTables().templates.items;
 
-        // The unique ID for the T-7 Thermal Goggles
+        // The ID for the T-7 Thermal Goggles
         const t7GogglesId = "5c0558060db834001b735271";
 
         // Find the T-7 goggles item in the database
-        const thermalItem = items[t7GogglesId];
-
-        if (thermalItem) {
-            this.logger.info(`[${this.modName}] Found and modifying: ${thermalItem._props.Name}`);
-
-            // 1. Make the image clearer by reducing noise.
-            // A value of 0.01 is very low, making the image crisp.
-            const originalNoise = thermalItem._props.NoiseIntensity;
-            thermalItem._props.NoiseIntensity = 0.01;
-            this.logger.info(`    - Noise Intensity changed from ${originalNoise} to ${thermalItem._props.NoiseIntensity}`);
-
-            // 2. Enable IR detection by adding the property.
-            const originalIrState = thermalItem._props.InfraredVision || false;
-            thermalItem._props.InfraredVision = true;
-            this.logger.info(`    - Infrared Vision changed from ${originalIrState} to ${thermalItem._props.InfraredVision}`);
+        const t7Goggles = items[t7GogglesId];
+        
+        // Check if the item exists and has the properties we want to change
+        if (t7Goggles && t7Goggles._props) 
+        {
+            // Lower the noise intensity to make the picture clearer
+            t7Goggles._props.NoiseIntensity = 0.01;
             
-            this.logger.info(`[${this.modName}] Successfully applied all thermal changes!`);
-        } else {
-            this.logger.error(`[${this.modName}] Could not find the T-7 Goggles (ID: ${t7GogglesId}) in the database.`);
+            // Add the InfraredVision property to see IR lasers/lights
+            t7Goggles._props.InfraredVision = true;
+
+            this.logger.info(`[${this.modName}] T-7 Goggles updated successfully!`);
+        }
+        else
+        {
+            this.logger.error(`[${this.modName}] Could not find T-7 Goggles with ID: ${t7GogglesId}`);
         }
     }
 }
 
-module.exports = { mod: new ThermalMod() };
+module.exports = { mod: new ClearThermals() }
 
